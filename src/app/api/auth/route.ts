9e972d98c5981a3d4f4dfefd1d679bd586b6d658
EstebanMa12 <desteban.maya@udea.edu.co>
2024-05-30 21:48:49 -0500

    chore: Update .gitignore to include .env file

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server"
+export function GET() {
+    // extract params
+    // query database
+    // Communicate with other APIs
+    // return response
+    return NextResponse.json({ message: "Hello from the API!" })
+}
