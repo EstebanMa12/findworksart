f4c1ed48bf6c33ac10d1c9e4f77706f0632a7b1e
EstebanMa12 <desteban.maya@udea.edu.co>
2024-05-31 14:11:48 -0500

    chore: Update npm dependencies, tailwind configuration, and add some components from shadcn

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import {Link} from "next/link"
+export default function NotFound(){
+    return (
+    <section className = "flex h-screen justify-center items-center"
+        >
+        <div>
+            <h1
+            className = "text-white text-4xl font-bold"
+            >Not Found</h1>
+            
+        </div>
+    </section>
+    )
+}
